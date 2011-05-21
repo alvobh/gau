a3a8df19c2f458852289a9499c1f8603848136ca
Mateus Galvão <mateus@uololou.com.br>
2011-05-21 01:49:33 -0300

    Just a refactorization

--- src/gau/Main.java
+++ src/gau/Main.java
@@ -12,33 +12,6 @@ public class Main {
        GuiMain gui = new GuiMain(); 
        gui.setVisible(true);
    }	
-	
-   private static void start() throws Exception{
-	   ArrayList<RealTeam> teams = getTeams();
-	   Game prova = new Game(teams);
-	   prova.startWithPlugs();
-   }
-   
-   private static ArrayList<RealTeam> getTeams () throws Exception{
-   
-      Scanner scanner = new Scanner(new File("times.txt"));  
-      ArrayList<RealTeam> teams = new ArrayList<RealTeam>();
-      RealTeam team = new RealTeam();
-      while ( scanner.hasNext() ) {  
-         String name = scanner.nextLine();
-         if (name.charAt(0) == ' ') {
-            Team aux = new Team (name.substring(1));
-            team.add(aux);
-         } else {
-            Team aux = new Team (name);
-            team = new RealTeam(aux);
-            teams.add(team);
-         }
-      }
-      scanner.close(); 
-      return teams;
-
-   }
 
 }
 
