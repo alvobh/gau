43d3263f1eaaaca8a0e1d469931639baf56c1bb0
Mateus Galvão <mateus@uololou.com.br>
2011-05-29 11:47:28 -0300

    Finished the DB layer

--- src/gau/state/DB.java
+++ src/gau/state/DB.java
@@ -20,7 +20,7 @@ class DB {
    private Statement query;
 
    public DB(final String file)
-      throws SQLException, ClassNotFoundException {
+         throws SQLException, ClassNotFoundException {
       Class.forName("org.sqlite.JDBC");
       db = DriverManager.getConnection("jdbc:sqlite:" + file);
       query = db.createStatement();
@@ -37,17 +37,22 @@ class DB {
             + "value VARCHAR(50) NOT NULL)");
    }
 
-   public final ResultSet query(final int id)
-         throws SQLException {
-      String inner = "SELECT key,value FROM data WHERE id=" + id,
-             statement = "SELECT i.key as 'rkey',d.id as 'id',"
-                          + "d.key as 'key',d.value as 'value' "
-                          + "FROM data as d, (" + inner + ") as i "
-                          + "WHERE i.value = d.id";
+   public final ResultSet get(final int id) throws SQLException {
+      String statement = "SELECT key,value "
+                       + "FROM data "
+                       + "WHERE id = " + id + " "
+                       + "ORDER BY key ASC";
       return query.executeQuery(statement);
    }
 
-  
+   public final ResultSet get(final String key) throws SQLException {
+      String statement = "SELECT id,value "
+                       + "FROM data "
+                       + "WHERE key = '" + key + "' "
+                       + "ORDER BY id ASC";
+      return query.executeQuery(statement);
+   }
+
    public final int insert(final String key, final String value)
          throws SQLException {
       String statement = "INSERT into data (id,key,value) "
@@ -58,7 +63,7 @@ class DB {
       ResultSet results = query.getGeneratedKeys();
       return results.getInt(1);
    }
-   
+
    public final int insert(final int id, final String key,
          final String value) throws SQLException {
       String statement = "INSERT into data (id,key,value) "
@@ -70,12 +75,18 @@ class DB {
       return results.getInt(1);
    }
 
-   private void update(final int id, final String key,
+   public final void update(final int id, final String key,
          final String value) throws SQLException {
-      String statement = "UPDATE things "
+      String statement = "UPDATE data "
                           + "SET value = '" + value + "' "
                           + "WHERE id = " + id + " "
                           + "AND key = '" + key + "'";
+      query.executeUpdate(statement);
+   }
+
+   public final void remove(final int id) throws SQLException {
+      String statement = "REMOVE FROM data "
+                          + "WHERE id = " + id + " ";
       query.executeUpdate(statement);
    }
 
